e01cea581a04ac430da137fdf60047fa559e783a
HAL-HatanakaYu <82099820 +HAL-HatanakaYu@users.noreply.github.com>
2021-10-06 18:20:25 +0900

    レティクル追加

---
+++ Project/Assets/Scripts/Reticle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+    private RectTransform reticle;
+
+    [Range(200f, 300f)]
+    public float size;
+
+    public float restingSize;
+    public float maxSize;
+    public float speed;
+
+    public float curtSize;
+
+    private void Start()
+    {
+        reticle = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        //playerのrigidbody.velocityでもいい
+        if (isMoving())
+        {
+            curtSize = Mathf.Lerp(curtSize, maxSize, Time.deltaTime * speed);
+        }
+        else
+        {
+            curtSize = Mathf.Lerp(curtSize, restingSize, Time.deltaTime * speed);
+        }
+
+        reticle.sizeDelta = new Vector2(curtSize, curtSize); 
+    }
+
+    bool isMoving()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}

b2e0c5862a6eb87dfb93d2db21cb467882fa50a3
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2021-10-07 13:40:19 +0900

    update

--- Project/Assets/Scripts/EnemySpawn.cs
+++ Project/Assets/Scripts/EnemySpawn.cs
@@ -6,27 +6,35 @@ public class EnemySpawn : MonoBehaviour
 {
     //敵プレハブ
     public GameObject enemyPrefab;
-    //敵生成時間間隔
-    private float interval;
+
     //経過時間
-    private float time = 0f;
+    private float time;
+
+    //配列サイズ
+    private int arraysize;
+
+    [SerializeField]
+    //スポーン時間
+    private float[] spawntime = new float[10];
 
     // Start is called before the first frame update
     void Start()
     {
-        interval = 3f;
+        arraysize = spawntime.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-
-        if (time > interval)
+        time = Time.frameCount % 60f;
+        
+        for(int i = 0; i < arraysize; i++)
         {
-            GameObject enemy = Instantiate(enemyPrefab);
-            enemy.transform.position = new Vector3(8.5f, 6.5f, 11.8f);
-            time = 0f;
+            if (spawntime[i] == time)
+            {
+                GameObject enemy = Instantiate(enemyPrefab);
+                enemy.transform.position = this.gameObject.transform.position;
+            }
         }
     }
 }

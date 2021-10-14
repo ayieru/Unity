18077d73719db03fd1fc9c04f1207b339bb33177
kawausokaitai <79248520+kawausokaitai@users.noreply.github.com>
2021-10-14 19:02:58 +0900

    タイトルとステージの作成とシーン移動作成

--- Project/Assets/Scripts/Title.cs
+++ Project/Assets/Scripts/Title.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;　//シーンマネージャー呼び出し
 
 public class Title : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      //  SceneManager.LoadScene("");
+
+    }
+
+    //スタートボタンを押すとセレクト画面に切り替わる
+    public void StartBt()
+    {
+        SceneManager.LoadScene("StageSelect");
     }
 }

966b06e3b9f0b69e54635b9862dd2599de04c592
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2021-10-15 11:04:02 +0900

    update

---
+++ Project/Assets/Scripts/ScriptableObject/W_Masterdata.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create WeaponMasterData")]
+
+public class W_MasterData : ScriptableObject
+{
+	static int count = 4;
+
+	public WeaponData[] data = new WeaponData[count];
+	public string[] weaponName = new string[count];
+}
--- Project/Assets/Scripts/ScriptableObject/WeaponData.cs
+++ Project/Assets/Scripts/ScriptableObject/WeaponData.cs
@@ -9,6 +9,8 @@ public class WeaponData : ScriptableObject
 	static int count = 3;
 
 	public GameObject weapon;
+	public Sprite image;
+	public string weaponName;
 	public int[]   level    = new int[count];
 	public int[]   damage   = new int[count];
 	public int[]   headShot = new int[count];

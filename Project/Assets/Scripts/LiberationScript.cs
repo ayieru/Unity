617e911b2557943783e411d6236b43918a52f1d0
kawausokaitai <79248520+kawausokaitai@users.noreply.github.com>
2021-10-20 16:22:28 +0900

    ステージ開放スクリプトかいた

---
+++ Project/Assets/Scripts/LiberationScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーンをロードする場合に必要
+
+// ゴールした時にstage_numの値が変更されるようにするためのスクリプト
+public class LiberationScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.name == "Player")
+    //    {
+    //        //PlayerPrefsのSCOREに3という値を入れる
+    //        PlayerPrefs.SetInt("SCORE", 3);
+    //        //PlayerPrefsをセーブする         
+    //        PlayerPrefs.Save();
+
+    //    }
+    //}
+    private int stageNum;
+    public void StageOpen()
+    {
+        PlayerPrefs.SetInt("stageNumber", stageNum);//PlayerPrefsでクリアしたステージ番号をセット
+    }
+
+    //public void push()
+    //{
+    //    //PlayerPrefsのSCOREに3という値を入れる
+    //    PlayerPrefs.SetInt("SCORE", 3);
+    //    //PlayerPrefsをセーブする         
+    //    PlayerPrefs.Save();
+
+    //    SceneManager.LoadScene("StageSelect");
+    //}
+}
---
+++ Project/Assets/Scripts/StageLiberation.cs
@@ -0,0 +1,119 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//// ステージが解放されるスクリプト
+//public class StageLiberation : MonoBehaviour
+//{
+
+//    public int stage_num; // スコア変数
+//    public GameObject ni;
+//    public GameObject san;
+//    public GameObject yon;
+//    public GameObject go;
+
+
+//    private void Go()
+//    {
+//        int clearLevel = PlayerPrefs.GetInt("SCORE", 0);
+
+//        //レベルに応じてUI画像入れ替え
+//        switch (clearLevel)
+//        {
+//            case 1:
+//                ni.SetActive(true);
+//                break;
+//            case 2:
+//                san.SetActive(true);
+//                break;
+//            case 3:
+//                yon.SetActive(true);
+//                break;
+//        }
+//    }
+
+//        // Use this for initialization
+//        void Start()
+//    {
+//        //現在のstage_numを呼び出す
+//        stage_num = PlayerPrefs.GetInt("SCORE", 0);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //stage_numが２以上のとき、ステージ２を解放する。以下同様
+//        //if (stage_num >= 2)
+//        //{
+//        //    ni.SetActive(true);
+//        //}
+
+//        //if (stage_num >= 3)
+//        //{
+//        //    san.SetActive(true);
+//        //}
+
+//        //if (stage_num >= 4)
+//        //{
+//        //    yon.SetActive(true);
+//        //}
+
+//        //if (stage_num >= 5)
+//        //{
+//        //    go.SetActive(true);
+//        //}
+
+//    }
+//}
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageLiberation : MonoBehaviour
+{
+    private int stageNumber; //ステージ開放条件の値を格納
+
+    //ステージ1はゲームスタート時に解放されているのでstage2から
+    public GameObject stage2;
+    public GameObject stage3;
+    public GameObject stage4;
+    public GameObject stage5;
+
+
+    void Start()
+    {
+        stageNumber = PlayerPrefs.GetInt("stageNumber");
+    }
+
+
+    void Update()
+    {
+        if (stageNumber >= 2)
+        {
+            stage2.SetActive(true);
+        }
+
+        if (stageNumber >= 3)
+        {
+            stage3.SetActive(true);
+        }
+
+        if (stageNumber >= 4)
+        {
+            stage4.SetActive(true);
+        }
+
+        if (stageNumber >= 5)
+        {
+            stage5.SetActive(true);
+        }
+
+
+    }
+}

7c70be45256c0781d8a130ac1d8de8a34fd3c263
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2021-10-21 10:27:13 +0900

    update

---
+++ Project/Assets/Scripts/Item/Turret.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Turret : MonoBehaviour
+{
+    private enum TurretAiState
+    {
+        SEARCH,          //探索
+        ATTACK,          //攻撃
+        IDLE,            //待機
+    }
+
+    public GameObject ammo;
+    public float NEAR_DIR = 0;
+    public AudioClip sound;
+
+    private GameObject p;
+    private TurretAiState aiState = TurretAiState.IDLE;
+    private TurretAiState nextState;
+    private AudioSource audioSource;
+    private bool search = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        InvokeRepeating(nameof(UpdateAI), 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void SetAi()
+    {
+        AiMainRoutine();
+        aiState = nextState;
+    }
+
+    private void AiMainRoutine()
+    {
+        if (search)
+        {
+            nextState = TurretAiState.ATTACK;
+        }
+        else
+        {
+            nextState = TurretAiState.SEARCH;
+        }
+    }
+
+    private void UpdateAI()
+    {
+        SetAi();
+        switch (aiState)
+        {
+            case TurretAiState.SEARCH:
+                Search();
+                break;
+            case TurretAiState.ATTACK:
+                Attack();
+                break;
+            case TurretAiState.IDLE:
+                Idle();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Search()
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject obj in targets)
+        {
+            float dir = Vector3.Distance(obj.transform.position, transform.position);
+            if (dir < NEAR_DIR)
+            {
+                search = true;
+                p = obj;
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        if (p != null)
+        {
+            transform.LookAt(p.transform);
+            Shoot();
+            audioSource.PlayOneShot(sound);
+        }
+        search = false;
+    }
+
+    private void Idle()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Shoot()
+    {
+        // 上で取得した場所に、"bullet"のPrefabを出現させる
+        GameObject newAmmo = Instantiate(ammo.gameObject, transform.position, transform.rotation);
+        ammo.GetComponent<Ammo>().SetPower(30);
+        // 出現させたボールのforward(z軸方向)
+        Vector3 direction = transform.forward;
+        // 弾の発射方向にnewBallのz方向(ローカル座標)を入れ、弾オブジェクトのrigidbodyに衝撃力を加える
+        newAmmo.GetComponent<Rigidbody>().AddForce(direction * 50, ForceMode.Impulse);
+        // 出現させたボールの名前を"bullet"に変更
+        newAmmo.name = ammo.gameObject.name;
+        // 出現させたボールを0.8秒後に消す
+        Destroy(newAmmo, 0.8f);
+    }
+}

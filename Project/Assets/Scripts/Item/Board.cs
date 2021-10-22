f5c2ce42952143ebedf17c4ec911b1e4e4cd490d
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2021-10-22 12:41:09 +0900

    update

---
+++ Project/Assets/Scripts/Item/Board.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour,IReceiveDamage
+{
+    public ItemData data;
+    private int hp;
+
+    void Start()
+    {
+        hp = data.hp[1];
+    }
+
+    public bool ReceiveDamage(int Pdamage)
+    {
+        bool deadFlag = false;
+        hp -= Pdamage;
+        if (hp <= 0)
+        {
+            hp = 0;
+            Destroy(gameObject);
+            deadFlag = true;
+        }
+
+        Debug.Log("Barricade は " + Pdamage + "ダメージ食らった\n残りHP " + hp);
+
+        return deadFlag;
+    }
+}

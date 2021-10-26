71c81c603f0f419a0d7f353421edc31ad5f92375
HAL-HatanakaYu <82099820 +HAL-HatanakaYu@users.noreply.github.com>
2021-10-26 18:38:42 +0900

    UIの武器強化、購入画面追加

--- Project/Assets/Scripts/UI/UIManager.cs
+++ Project/Assets/Scripts/UI/UIManager.cs
@@ -5,133 +5,128 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    //Panelオブジェクトをセットする
-    [SerializeField] GameObject P_Player;
-    [SerializeField] GameObject P_ShopChoice;
-    [SerializeField] GameObject P_WeaponShop;
-    [SerializeField] GameObject P_ObjectShop;
-    [SerializeField] GameObject P_Update;
-    [SerializeField] GameObject P_GameClear;
-    [SerializeField] GameObject P_GameOver;
-
-    //現在のUIの状態保存
-    enum UI_State{
+       //現在のUIの状態保存
+    public enum UI_State{
         Player,
         ShopChoice,
-        WeaponShop,
+        WeaponMenu,
         ObjectShop,
+        WeaponPurchase,
+        WeaponUpdate,
+        Max
     }
-    UI_State state;
+    public UI_State state;
+    //Panelオブジェクトをセットする
+    [SerializeField] GameObject[] GOState = new GameObject[(int)UI_State.Max];
 
     bool cursorLock = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        SetPlayerUI();
+        state = UI_State.Player;
+        GOState[(int)state].SetActive(true);
+        for(int i = 1; i < (int)UI_State.Max; i++)
+        {
+            GOState[i].SetActive(false);
+        }
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.Return))
         {
-            SetWeapon_or_Object();
+            SetUIState(UI_State.ShopChoice);
+            cursorLock = false;
             UpdareCursorLock();
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            switch (state)
-            {
-                case UI_State.ShopChoice:
-                    SetPlayerUI();
-                    break;
-
-                case UI_State.WeaponShop:
-                    SetWeapon_or_Object();
-                    break;
-
-                case UI_State.ObjectShop:
-                    SetWeapon_or_Object();
-                    break;
-
-                default:
-                    break;
-            }
+            ReturnUI();
         }
         UpdareCursorLock();
+    }
+
+    // UI画面変更
+    void SetUIState(UI_State state)
+    {
+        GOState[(int)this.state].SetActive(false);
+        this.state = state;
+        GOState[(int)this.state].SetActive(true);
+    }
+
+    //UI戻るときの関数
+    void ReturnUI()
+    {
+        if(state != UI_State.Player){
+            if(state == UI_State.WeaponMenu || state == UI_State.ObjectShop)
+            {
+                SetUIState(UI_State.ShopChoice);
+            }
+            else if(state == UI_State.WeaponPurchase || 
+                state == UI_State.WeaponUpdate)
+            {
+                SetUIState(UI_State.WeaponMenu);
+            }
+            else
+            {
+                UI_State tmpState = this.state - 1;
+                SetUIState(tmpState);
+            }
+        }
+        else 
+        {
+            cursorLock = false;
+        }        
     }
 
     //プレイヤー画面
     public void SetPlayerUI()
     {
-        state = UI_State.Player;
-        cursorLock = true;
-
-        P_Player.SetActive(true);
-        P_ShopChoice.SetActive(false);
-        P_WeaponShop.SetActive(false);
-        P_ObjectShop.SetActive(false);
-        P_Update.SetActive(false);
-        P_GameClear.SetActive(false);
-        P_GameOver.SetActive(false);
+        SetUIState(UI_State.Player);
     }
 
     //武器or配置物選択画面
     public void SetWeapon_or_Object()
     {
-        state = UI_State.ShopChoice;
-        cursorLock = false;
-
-        P_Player.SetActive(false);
-        P_ShopChoice.SetActive(true);
-        P_WeaponShop.SetActive(false);
-        P_ObjectShop.SetActive(false);
-        P_Update.SetActive(false);
+        SetUIState(UI_State.ShopChoice);
     }
 
+    public void SetWeaponPurchase()
+    {
+        SetUIState(UI_State.WeaponPurchase);
+    }
+    public void SetWeaponUpdate()
+    {
+        SetUIState(UI_State.WeaponUpdate);
+    }
+
+    //武器の強化or購入画面
+    public void SetWeaponMenu()
+    {
+        SetUIState(UI_State.WeaponMenu);
+    }
     //武器選択画面
     public void SelectWeapon()
     {
-        state = UI_State.WeaponShop;
-
-        P_ShopChoice.SetActive(false);
-        P_WeaponShop.SetActive(true);
+        SetUIState(UI_State.WeaponMenu);
     }
 
     //配置物選択画面
     public void SelectObject()
     {
-        state = UI_State.ObjectShop;
-
-        P_ShopChoice.SetActive(false);
-        P_ObjectShop.SetActive(true);
+        SetUIState(UI_State.ObjectShop);
     }
 
     public void GameOver()
     {
         cursorLock = false;
-
-        P_Player.SetActive(false);
-        P_ShopChoice.SetActive(false);
-        P_WeaponShop.SetActive(false);
-        P_ObjectShop.SetActive(false);
-        P_Update.SetActive(false);
-        P_GameClear.SetActive(false);
-        P_GameOver.SetActive(true);
     }
 
     public void GameClear()
     {
         cursorLock = false;
-
-        P_Player.SetActive(false);
-        P_ShopChoice.SetActive(false);
-        P_WeaponShop.SetActive(false);
-        P_ObjectShop.SetActive(false);
-        P_Update.SetActive(false);
-        P_GameClear.SetActive(true);
-        P_GameOver.SetActive(false);
     }
 
     private void UpdareCursorLock()

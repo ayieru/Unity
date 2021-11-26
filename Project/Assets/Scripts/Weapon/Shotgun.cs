7ac8ba8f0b99b06971fb5c968aaaf94d4efb5247
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2021-11-26 22:55:46 +0900

    update

--- Project/Assets/Scripts/Weapon/Shotgun.cs
+++ Project/Assets/Scripts/Weapon/Shotgun.cs
@@ -36,27 +36,28 @@ public class Shotgun : Weapon
                     MazzuleFlash.Play();
 
                     Vector3 direction = GetFPForward();
-                    // 上で取得した場所に、"bullet"のPrefabを出現させる
-                    for (int cnt = 0; cnt < 1; cnt++)
+                    Vector3 force = new Vector3(0f, 0f, 0f);
+
+                    //フォース変更ｙ、シェルのランダム生成、方向ベクトル
+
+                    for (int cnt = 0; cnt < 10; cnt++)
                     {
                         float random1 = Random.Range(0.1f, -0.1f);
                         float random2 = Random.Range(0.1f, -0.1f);
                         float random3 = Random.Range(0.1f, -0.1f);
-                        Vector3 force = new Vector3(random1, random2, random3);
+                        force = new Vector3(random1, random2, random3);
+
                         GameObject bullet = Instantiate(magazine.bullet.gameObject, GetFPPosition(), magazine.bullet.transform.rotation);
                         magazine.bullet.GetComponent<Bullet>().SetPower((int)power);
                         Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
-                        bulletRb.AddForce(direction * power, ForceMode.Impulse);
+                        bulletRb.AddForce(direction/100, ForceMode.Impulse);
                         bulletRb.AddForce(force);
-                        Debug.Log(force);
                     }
 
                     GameObject newShell = Instantiate(magazine.shell.gameObject, magazine.transform.position, magazine.shell.transform.rotation);
                     // 弾の発射方向にnewBallのz方向(ローカル座標)を入れ、弾オブジェクトのrigidbodyに衝撃力を加える
                     newShell.GetComponent<Rigidbody>().AddForce(direction * 1f, ForceMode.Impulse);
-                    // 出現させたボールの名前を"bullet"に変更
-                    //newBullet.GetComponent<Bullet>().playerScript = playerScript;
-                    //newBullet.name = magazine.bullet.gameObject.name;
+
                     shootFlag = false;
                     shootingFrag = true;
                 }

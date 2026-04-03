efc59889900755b9b971bb4690020f24e70b59eb
yuゆ様 <numberrey.0418@icloud.com>
2026-04-03 15:30:45 +0900

    delete_log

--- Project/Assets/Scripts/GameFlow/StageLiberation.cs
+++ Project/Assets/Scripts/GameFlow/StageLiberation.cs
@@ -1,75 +1,4 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-
-//// ステージが解放されるスクリプト
-//public class StageLiberation : MonoBehaviour
-//{
-
-//    public int stage_num; // スコア変数
-//    public GameObject ni;
-//    public GameObject san;
-//    public GameObject yon;
-//    public GameObject go;
-
-
-//    private void Go()
-//    {
-//        int clearLevel = PlayerPrefs.GetInt("SCORE", 0);
-
-//        //レベルに応じてUI画像入れ替え
-//        switch (clearLevel)
-//        {
-//            case 1:
-//                ni.SetActive(true);
-//                break;
-//            case 2:
-//                san.SetActive(true);
-//                break;
-//            case 3:
-//                yon.SetActive(true);
-//                break;
-//        }
-//    }
-
-//        // Use this for initialization
-//        void Start()
-//    {
-//        //現在のstage_numを呼び出す
-//        stage_num = PlayerPrefs.GetInt("SCORE", 0);
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        //stage_numが２以上のとき、ステージ２を解放する。以下同様
-//        //if (stage_num >= 2)
-//        //{
-//        //    ni.SetActive(true);
-//        //}
-
-//        //if (stage_num >= 3)
-//        //{
-//        //    san.SetActive(true);
-//        //}
-
-//        //if (stage_num >= 4)
-//        //{
-//        //    yon.SetActive(true);
-//        //}
-
-//        //if (stage_num >= 5)
-//        //{
-//        //    go.SetActive(true);
-//        //}
-
-//    }
-//}
-
-
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -107,8 +36,6 @@ public class StageLiberation : MonoBehaviour
     {
         int stageNumber = PlayerPrefs.GetInt("stageNumber");
 
-        Debug.Log(PlayerPrefs.GetInt("stageNumber"));
-
         if (stageNumber >= 2)
         {
             stage2.SetActive(true);
@@ -128,7 +55,5 @@ public class StageLiberation : MonoBehaviour
         {
             stage5.SetActive(true);
         }
-
-
     }
 }
